aafc772b49763330c829857d8715ffc0f3382299
Miguel Gila <miguel.gila@cscs.ch>
2023-11-17 18:47:58 -0700

    Cleanup the authentication call, and test for object removal.

--- src/shasta/ims/s3_test.rs
+++ src/shasta/ims/s3_test.rs
@@ -2,6 +2,8 @@ use tempfile::{NamedTempFile};
 use std::env::temp_dir;
 use std::io::{Read, Write};
 use crate::shasta::ims::s3::s3::{s3_auth, s3_download_object, s3_remove_object, s3_upload_object};
+use std::error::Error;
+use serde_json::Value;
 
 pub const TOKEN_VAR_NAME:&str = "MANTA_CSM_TOKEN";
 pub const API_URL_VAR_NAME:&str = "MANTA_TEST_API_URL";
@@ -28,19 +30,17 @@ pub const OBJECT_PATH:&str = "manta-test-2-delete/dummy.txt";
 ///
 /// ref -> https://cray-hpe.github.io/docs-csm/en-13/operations/artifact_management/generate_temporary_s3_credentials/
 
+async fn authenticate_with_s3() -> anyhow::Result<Value, Box<dyn Error>> {
+    let shasta_token = std::env::var(TOKEN_VAR_NAME).unwrap();
+    let shasta_base_url = std::env::var(API_URL_VAR_NAME).unwrap();
+    s3_auth(&shasta_token, &shasta_base_url).await
+}
+
 #[tokio::test]
 pub async fn test_1_s3_auth() {
     println!("----- TEST S3 AUTH -----");
-
-    let shasta_token = std::env::var(TOKEN_VAR_NAME).unwrap();
-    let shasta_base_url = std::env::var(API_URL_VAR_NAME).unwrap();
-
-    let _sts_value = match s3_auth(&shasta_token, &shasta_base_url).await {
-        // Ok(sts_value) => sts_value,
-        Ok(_sts_value) => {
-                println!("Debug - STS token:\n{:#?}", _sts_value);
-                assert!(true)
-        }
+    let _result = match authenticate_with_s3().await {
+        Ok(_result) => assert!(true),
         Err(error) => assert!(false,"Error getting temporary s3 token from STS. Error returned: '{}'", error),
     };
 }
@@ -50,6 +50,9 @@ pub async fn test_1_s3_auth() {
 pub async fn test_2_s3_put_object() {
     // tracing_subscriber::fmt::init();
     println!("----- TEST S3 PUT OBJECT -----");
+
+    let bucket_name = BUCKET_NAME;
+    let object_path = OBJECT_PATH;
 
     // create dummy file on the local filesystem
     let mut file1 = match NamedTempFile::new() {
@@ -77,20 +80,13 @@ pub async fn test_2_s3_put_object() {
 
     // Connect and auth to S3
 
-    let shasta_token = std::env::var(TOKEN_VAR_NAME).unwrap();
-    let shasta_base_url = std::env::var(API_URL_VAR_NAME).unwrap();
-    let bucket_name = BUCKET_NAME;
-    let object_path = OBJECT_PATH;
 
-    let sts_value = match s3_auth(&shasta_token, &shasta_base_url).await {
-        // Ok(sts_value) => sts_value,
+    let sts_value = match authenticate_with_s3().await {
         Ok(sts_value) => {
             println!("Debug - STS token:\n{:#?}", sts_value);
             sts_value
-        },
-        Err(error) => {
-            panic!("{}", error.to_string())
-        },
+        }
+        Err(error) => panic!("{}", error.to_string())
     };
 
     // Upload dummy file
@@ -103,37 +99,21 @@ pub async fn test_2_s3_put_object() {
         },
         Err(error) => assert!(false, "Error {}", error.to_string())
     };
-    // Cleanup
-    // Don't need to remove the temporary file, but it needs to be pulled off s3
-    // let _result = match s3_remove_object(&sts_value,
-    //                                         &object_path,
-    //                                         &bucket_name
-    //                                         ) {
-    //     Ok(_result) => {
-    //         println!("File removed successfully from s3.");
-    //     },
-    //     Err(error) => assert!(false, "Error {}", error.to_string())
-    // };
 }
 
 #[tokio::test]
 pub async fn test_3_s3_get_object() {
     println!("----- TEST S3 GET OBJECT -----");
 
-    let shasta_token = std::env::var(TOKEN_VAR_NAME).unwrap();
-    let shasta_base_url = std::env::var(API_URL_VAR_NAME).unwrap();
     let object_path = OBJECT_PATH;
     let bucket_name = BUCKET_NAME;
 
-    let sts_value = match s3_auth(&shasta_token, &shasta_base_url).await {
-        // Ok(sts_value) => sts_value,
+    let sts_value = match authenticate_with_s3().await {
         Ok(sts_value) => {
             println!("Debug - STS token:\n{:#?}", sts_value);
             sts_value
-        },
-        Err(error) => {
-            panic!("{}", error.to_string())
-        },
+        }
+        Err(error) => panic!("{}", error.to_string())
     };
 
     let destination_path:String = temp_dir().join(object_path).display().to_string();
@@ -148,4 +128,33 @@ pub async fn test_3_s3_get_object() {
         Err(error) => assert!(false, "Error {}", error.to_string())
     };
     assert!(true, "OK all files completed downloading.")
+}
+
+
+#[tokio::test]
+pub async fn test_4_s3_remove_object() {
+    println!("----- TEST S3 REMOVE OBJECT -----");
+
+    let object_path = OBJECT_PATH;
+    let bucket_name = BUCKET_NAME;
+
+    let sts_value = match authenticate_with_s3().await {
+        Ok(sts_value) => {
+            println!("Debug - STS token:\n{:#?}", sts_value);
+            sts_value
+        }
+        Err(error) => panic!("{}", error.to_string())
+    };
+
+    println!("Removing file {}/ {}", &bucket_name, &object_path);
+
+    let _result = match s3_remove_object(&sts_value,
+                                           &object_path,
+                                           &bucket_name).await {
+        Ok(_result) => {
+            println!("Object deletion completed.");
+        },
+        Err(error) => assert!(false, "Error {}", error.to_string())
+    };
+    assert!(true, "OK, the file was removed successfully.")
 }
