ef7b7c8e3e4a160f132b03e51e25e547f50f9688
Miguel Gila <miguel.gila@cscs.ch>
2024-05-30 15:04:32 +0200

    actual fix for when creating a new HSM group

--- src/hsm.rs
+++ src/hsm.rs
@@ -306,18 +306,41 @@ pub mod group {
 
             let url_api = shasta_base_url.to_owned() + "/smd/hsm/v2/groups";
 
-            let result = client
+            // let result = client
+            //     .post(url_api)
+            //     .header("Authorization", format!("Bearer {}", shasta_token))
+            //     .json(&hsm_group_json) // make sure this is not a string!
+            //     .send()
+            //     .await
+            //     .expect("failed to get response")
+            //     .text()
+            //     .await
+            //     .expect("failed to get payload");
+            let result = match client
                 .post(url_api)
                 .header("Authorization", format!("Bearer {}", shasta_token))
                 .json(&hsm_group_json) // make sure this is not a string!
                 .send()
                 .await?
-                // .expect("failed to get response")
+                .error_for_status()?
                 .text()
-                .await?;
-                // .expect("failed to get payload");
-            // if we reach this point, the previous call hasn't bailed on us
+                .await {
+
+                Ok(_res) => {
+                    println!("Result:{}",_res);
+                    _res
+                },
+                Err(e) => {
+                    println!("error: {}", e);
+                    return Ok(vec![hsm_group_json])
+                }
+            };
             Ok(vec![hsm_group_json])
+
+            // // if we reach this point, the previous call hasn't bailed on us
+            // println!("Result: {:?}",result);
+            // Ok(vec![hsm_group_json])
+
         }
 
         pub async fn delete_hsm_group(
