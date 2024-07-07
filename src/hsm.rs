4d57541f171bd21fbab985bac3e7b0189f9be4c1
Manuel Sopena Ballesteros <manuel.sopena@cscs.ch>
2024-07-07 03:44:06 +0200

    feat: add function to filter system hsm groups (eg alps, prealps, alpsm, alpse, alpsb, etc)

--- src/hsm.rs
+++ src/hsm.rs
@@ -393,6 +393,19 @@ pub mod group {
                 hsm_group_vec_filtered_rslt
             }
         }
+
+        pub fn filter_system_hsm_group_names(hsm_group_name_vec: Vec<String>) -> Vec<String> {
+            //TODO: Get rid of this by making sure CSM admins don't create HSM groups for system
+            //wide operations instead of using roles
+            let hsm_group_to_ignore_vec = ["alps", "prealps", "alpse", "alpsb"];
+
+            hsm_group_name_vec
+                .into_iter()
+                .filter(|hsm_group_name| {
+                    !hsm_group_to_ignore_vec.contains(&hsm_group_name.as_str())
+                })
+                .collect()
+        }
     }
 
     pub mod utils {
