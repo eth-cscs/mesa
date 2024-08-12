2566af3861e48717ce11b067da57f0e7f813351d
Manuel Sopena Ballesteros <manuel.sopena@cscs.ch>
2024-08-12 02:22:38 +0200

    feat: add function to migrate nodes between 2 different hsm groups

--- src/hsm.rs
+++ src/hsm.rs
@@ -501,9 +501,115 @@ pub mod group {
                 http_client::{get, post_member},
                 r#struct::HsmGroup,
             },
+            node::utils::validate_xnames,
         };
 
         use super::http_client::{self, delete_member};
+
+        pub async fn migrate_hsm_members(
+            shasta_token: &str,
+            shasta_base_url: &str,
+            shasta_root_cert: &[u8],
+            target_hsm_group_name: &str,
+            parent_hsm_group_name: &str,
+            new_target_hsm_members: Vec<&str>,
+            nodryrun: bool,
+        ) {
+            if !validate_xnames(
+                shasta_token,
+                shasta_base_url,
+                shasta_root_cert,
+                new_target_hsm_members.as_slice(),
+                Some(&parent_hsm_group_name.to_string()),
+            )
+            .await
+            {
+                eprintln!("Nodes '{:?}' not valid", new_target_hsm_members);
+                std::process::exit(1);
+            }
+
+            // get list of target HSM group members
+            let mut target_hsm_group_member_vec: Vec<String> = get_member_vec_from_hsm_group_name(
+                shasta_token,
+                shasta_base_url,
+                shasta_root_cert,
+                target_hsm_group_name,
+            )
+            .await;
+
+            // merge HSM group list with the list of xnames provided by the user
+            target_hsm_group_member_vec
+                .extend(new_target_hsm_members.iter().map(|xname| xname.to_string()));
+
+            target_hsm_group_member_vec.sort();
+            target_hsm_group_member_vec.dedup();
+
+            // get list of parent HSM group members
+            let mut parent_hsm_group_member_vec: Vec<String> = get_member_vec_from_hsm_group_name(
+                shasta_token,
+                shasta_base_url,
+                shasta_root_cert,
+                parent_hsm_group_name,
+            )
+            .await;
+
+            parent_hsm_group_member_vec
+                .retain(|parent_member| !target_hsm_group_member_vec.contains(parent_member));
+
+            parent_hsm_group_member_vec.sort();
+            parent_hsm_group_member_vec.dedup();
+
+            let target_hsm_group = serde_json::json!({
+                "label": target_hsm_group_name,
+                "decription": "",
+                "members": target_hsm_group_member_vec,
+                "tags": []
+            });
+
+            println!(
+                "{}",
+                serde_json::to_string_pretty(&target_hsm_group).unwrap()
+            );
+
+            let parent_hsm_group = serde_json::json!({
+                "label": parent_hsm_group_name,
+                "decription": "",
+                "members": parent_hsm_group_member_vec,
+                "tags": []
+            });
+
+            println!(
+                "{}",
+                serde_json::to_string_pretty(&parent_hsm_group).unwrap()
+            );
+
+            // *********************************************************************************************************
+            // UPDATE HSM GROUP MEMBERS IN CSM
+            if !nodryrun {
+                log::info!("Dryrun enabled, not modifying the HSM groups on the system.")
+            } else {
+                for xname in new_target_hsm_members {
+                    // TODO: This is creating a new client per xname, look whether this can be improved reusing the client.
+                    let _ = post_member(
+                        shasta_token,
+                        shasta_base_url,
+                        shasta_root_cert,
+                        target_hsm_group_name,
+                        xname,
+                    )
+                    .await;
+
+                    let _ = delete_member(
+                        shasta_token,
+                        shasta_base_url,
+                        shasta_root_cert,
+                        parent_hsm_group_name,
+                        xname,
+                    )
+                    .await;
+                }
+            }
+        }
 
         pub async fn update_hsm_group_members(
             shasta_token: &str,
